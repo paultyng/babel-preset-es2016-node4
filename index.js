8d243b462d7a4e706f4d774a8683033bffe2966b
Yad Smood <y.s.inside@gmail.com>
2016-04-01 11:18:12 +0800

    6.0.1

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ module.exports = {
     require("babel-plugin-transform-es2015-spread"),
     require("babel-plugin-transform-es2015-sticky-regex"),
     require("babel-plugin-transform-es2015-unicode-regex"),
-    require("babel-plugin-transform-async-to-generator")
+    require("babel-plugin-transform-async-to-generator"),
+    require("babel-plugin-syntax-trailing-function-commas"),
+    require("babel-plugin-transform-object-rest-spread"),
+    require("babel-plugin-transform-class-properties")
   ]
 };
